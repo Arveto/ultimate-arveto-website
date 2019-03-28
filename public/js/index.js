49d68c167fb9e800a41821e93330856dd45d78b3
PurplePachyderm <a.delval87@gmail.com>
2019-03-28 23:14:01 +0100

    Final hero repair

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,18 @@ let lang = navigator.language;
 
 (function () {
 
-    translate();
+
+    if(lang!="fr"){
+        translate()
+    }
+
+    else{
+        $('.fr').css('display', 'block').css('visibility', 'visible');
+        $('.en').css('display', 'none').css('visibility', 'none');
+        lang = 'fr'
+    }
+
+
 
 }())
 
@@ -12,12 +23,12 @@ $('#translate').on('click', translate);
 
 function translate() {
   if (lang == 'fr'){
-    print("1");
+
     $('.en').css('display', 'block').css('visibility', 'visible');
     $('.fr').css('display', 'none').css('visibility', 'none');
     lang = 'en'
   } else {
-    print("2");
+
     $('.fr').css('display', 'block').css('visibility', 'visible');
     $('.en').css('display', 'none').css('visibility', 'none');
     lang = 'fr'
