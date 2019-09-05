fda5ecc4695722117547eec089c3b8833d232a13
TERRUSS <olivier.benaben@protonmail.com>
2019-09-05 10:46:08 +0200

    added a dev server

---
+++ dev_app.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const http = require("http").Server(app);
+// const https = require('https');
+const path = require('path');
+/*
+const fs = require('fs');
+
+let options = {
+	key: fs.readFileSync('/etc/letsencrypt/live/arveto.io/privkey.pem', 'utf8'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/arveto.io/fullchain.pem', 'utf8'),
+}
+*/
+/******************************************************************************/
+
+app.use(require('express').static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/app', (req, res) => {
+    res.sendFile(__dirname + '/public/app.html');
+});
+
+http.listen(8080);
+/*
+https.createServer(options, app).listen(443);
+
+http.createServer(function (req, res) {
+	res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+	res.end();
+}).listen(80);
+*/
